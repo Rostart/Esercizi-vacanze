6b1eb8a33bdcb26f7a64b5293110341ec826e45a
Rosario Fortunato <rosariofortunato@Air-di-Rosario.home-life.hub>
2023-12-27 12:50:55 +0100

    mostra

---
+++ bottone-mostra-nascondi/scripts.js
@@ -0,0 +1,18 @@
+let button = document.getElementById("mostraParagrafo");
+let par = document.getElementById("paragrafo");
+
+
+function MostraParagrafo () {
+    par.style.display = "block";
+
+
+    }
+  
+    
+    
+    
+    button.addEventListener("click", MostraParagrafo);
+    
+
+  
+
