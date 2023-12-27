d3a3deda4362ae7383670ef3f586694fb87f3ada
Rosario Fortunato <rosariofortunato@Air-di-Rosario.home-life.hub>
2023-12-27 13:19:33 +0100

    dimensione

---
+++ cambia-dimensione-img/scripts.js
@@ -0,0 +1,10 @@
+let button = document.getElementById("immagine");
+let img = document.getElementById("Palermo");
+
+function CambiaDimensione () {
+img.style.width = "200px";
+
+
+}
+
+button.addEventListener("click", CambiaDimensione);
