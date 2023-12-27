53f0b3a9da7a319ad08f0e68485340c49291a62b
Rosario Fortunato <rosariofortunato@Air-di-Rosario.home-life.hub>
2023-12-27 11:23:21 +0100

    agg-element

---
+++ aggiungi-elemento/script.js
@@ -0,0 +1,5 @@
+let nuovoParagrafo = document.createElement('p');
+        nuovoParagrafo.textContent = "Forza Palermo";
+
+        let elementoPadre = document.getElementById('contenitore');
+        elementoPadre.appendChild(nuovoParagrafo);
